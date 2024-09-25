2af40cbac274698cafef5c70b15aaec662f644e1
SidVeld <sidveld@gmail.com>
2024-09-25 13:16:02 +0300

    fix(vscode): temporary extension disable

--- modules/home/editors/vscode.nix
+++ modules/home/editors/vscode.nix
@@ -2,10 +2,11 @@
 {
   programs.vscode = {
     enable = true;
-    extensions = with pkgs.vscode-extensions; [
-      nootyheen.nix-ide
-      alexandernanberg.horizon-theme-vscode
-    ];
+
+    # extensions = with pkgs.vscode-extensions; [
+    #   "nootyheen.nix-ide"
+    #   "alexandernanberg.horizon-theme-vscode"
+    # ];
 
     userSettings = {
       "editor.fontFamily" = "'FiraCode Nerd Font";

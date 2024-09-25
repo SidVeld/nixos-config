37a86bac8a91b9e1e06514c460cce401cf01aaa0
SidVeld <sidveld@gmail.com>
2024-09-25 13:12:00 +0300

    feat(boot): use systemd boot loader

--- modules/core/boot.nix
+++ modules/core/boot.nix
@@ -1,11 +1,6 @@
-
-
 { ... }:
 {
-  boot.loader.grub = {
-    enable = true;
-    devices = [ "nodev" ];
-    efiSupport = true;
-    useOSProber = true;
-  };
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.systemd-boot.configurationLimit = 10;
+  boot.loader.efi.canTouchEfiVariables = true;
 }

152e730031af0537b8908b6c4bfd8ee08644946c
SidVeld <sidveld@gmail.com>
2024-10-09 16:28:14 +0300

    feat: kitty tabs configuration

--- modules/home/kitty.nix
+++ modules/home/kitty.nix
@@ -3,13 +3,21 @@
   programs.kitty = {
     enable = true;
 
-    # font = {
-      # "FiraCode Nerd Font";
-      # size = 14;
-    # };
+    font = {
+      name = "FiraCode Nerd Font";
+      size = 14;
+    };
 
     settings = {
       confirm_os_window_close = 0;
+      background_opacity = "0.75";
+    };
+
+    keybindings = {
+      "alt+1" = "goto_tab 1";
+      "alt+2" = "goto_tab 2";
+      "alt+3" = "goto_tab 3";
+      "alt+4" = "goto_tab 4";
     };
   };
 }

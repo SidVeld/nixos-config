9838ba9adf81f0d41ac80f2ed1217d005f4f2f15
SidVeld <sidveld@gmail.com>
2024-10-11 18:23:22 +0300

    feat: monitors

--- modules/home/hyprland/config.nix
+++ modules/home/hyprland/config.nix
@@ -2,6 +2,12 @@
 {
   wayland.windowManager.hyprland = {
     settings = {
+
+      "monitor" = ", 1920x1080, 0x0, 1";
+      "monitor" = "DP-1, 2560x1440@60, 0x0, 1";
+      "monitor" = "DP-2, 2560x1440@60, 0x0, 1";
+      "monitor" = "DP-3, 2560x1440@60, 0x0, 1";
+
       exec-once = [
         "waybar &"
       ];
